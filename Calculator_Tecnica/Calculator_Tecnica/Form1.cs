43b04b4256eec71c0d87557f659a0a9da4831906
RafaLNunes <rafaf.code@gmail.com>
2024-02-05 08:28:39 -0300

    s

---
+++ Calculator_Tecnica/Calculator_Tecnica/Form1.cs
@@ -0,0 +1,10 @@
+namespace Calculator_Tecnica
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- Calculator_Tecnica/Calculator_Tecnica/obj/Debug/net6.0-windows/Calculator_Tecnica.AssemblyInfo.cs
+++ Calculator_Tecnica/Calculator_Tecnica/obj/Debug/net6.0-windows/Calculator_Tecnica.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Projeto Calculator")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Calculator_Tecnica")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cd833719dde232dd0f641ad107d3ae97fbd090f")]
-[assembly: System.Reflection.AssemblyProductAttribute("Projeto Calculator")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Projeto Calculator")]
+[assembly: System.Reflection.AssemblyProductAttribute("Calculator_Tecnica")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Calculator_Tecnica")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
